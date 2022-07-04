41e63bc45ec01a22948488c8519779e86a880e2f
Arantuil <gideon.t.v.d.k@hotmail.com>
2022-07-05 00:50:56 +0200

    sortable table with static mockdata

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,36 +1,70 @@
-import Cryptosoots from '../nftdata/cryptosoots/Cryptosoots'
+import Cryptosoots from '../nftdata/cryptosoots/Cryptosoots';
+import { useState } from 'react';
+import NFTdata from './NFTdata.json'
 
 function Table() {
     //let arrayalldata = Cryptosoots `${Cryptosoots}`
 
     //console.log(arrayalldata)
+    const [data, setdata] = useState(NFTdata)
+    const [order, setOrder] = useState("ASC")
+    const sorting =(col)=> {
+        if (col === "rank" || col === "price" || col === "marketcap") {
+            if (order === "ASC"){
+                const sorted = [...data].sort((a,b)=>
+                a[col].toString().localeCompare(b[col].toString(), "en", {
+                    numeric: true
+                }))
+                setdata(sorted);    
+                setOrder("DSC");
+            }
+            if (order === "DSC"){
+                const sorted = [...data].sort((a,b)=>
+                b[col].toString().localeCompare(a[col].toString(), "en", {
+                    numeric: true
+                }))
+                setdata(sorted);    
+                setOrder("ASC");
+            }
+        }
+        else {
+            if (order === "ASC"){
+                const sorted = [...data].sort((a,b)=>
+                    a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+                );
+                setdata(sorted);
+                setOrder("DSC");
+            }
+            if (order === "DSC"){
+                const sorted = [...data].sort((a,b)=>
+                    a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
+                );
+                setdata(sorted);
+                setOrder("ASC");
+            }
+        }
+    }
 
     return (
         <table className="table-auto w-full text-textprimary dark:text-darktextprimary transition-all">
             <thead>
                 <Cryptosoots></Cryptosoots>
                 <tr>
-                    <th>Song</th>
-                    <th>Artist</th>
-                    <th>Year</th>
+                    <th onClick={()=>sorting("rank")} className='text-left'>Rank</th>
+                    <th onClick={()=>sorting("name")} className='text-left'>NFT</th>
+                    <th onClick={()=>sorting("price")} className='text-left'>Floorprice</th>
+                    <th onClick={()=>sorting("marketcap")} className='text-left'>Marketcap</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    <td>Malcolm Lockyer</td>
-                    <td>1961</td>
-                </tr>
-                <tr>
-                    <td>Witchy Woman</td>
-                    <td>The Eagles</td>
-                    <td>1972</td>
-                </tr>
-                <tr>
-                    <td>Shining Star</td>
-                    <td>Earth, Wind, and Fire</td>
-                    <td>1975</td>
-                </tr>
+                { data.map((d)=>(
+                    <tr key={d.id}>
+                        <td>{d.rank}</td>
+                        <td>{d.name}</td>
+                        <td>{d.price}</td>
+                        <td>{d.marketcap}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     );
